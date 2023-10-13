02509baf988e14da13f063d95df5b25a8841efc5
dungpro1572000 <dungnt1572000@gmail.com>
2023-10-13 09:09:27 +0700

    add dependency, icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.vcare_app"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.vcare_app"
@@ -36,6 +36,21 @@ android {
 }
 
 dependencies {
+
+    /// RxJava and RxAndroid
+
+    implementation("io.reactivex.rxjava3:rxandroid:3.0.2")
+    // Because RxAndroid releases are few and far between, it is recommended you also
+    // explicitly depend on RxJava's latest version for bug fixes and new features.
+    // (see https://github.com/ReactiveX/RxJava/releases for latest 3.x.x version)
+    implementation("io.reactivex.rxjava3:rxjava:3.1.5")
+
+    /// Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+
+    // Gson convert
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
